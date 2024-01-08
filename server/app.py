150e1af227ce648bc6c7c1d9dff74f28dc73f3ad
leonkinyanjui <dapijon@gmail.com>
2024-01-09 00:50:19 +0300

    assignment complete. sorta!

--- server/app.py
+++ server/app.py
@@ -3,6 +3,18 @@
 from flask import Flask
 
 app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+@app.route('/print/<parameter>')
+def print_parameter(parameter):
+    print(parameter)
+    return parameter
+
+@app.route('/count/<parameter>')
+def count_parameter(parameter):
+    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
